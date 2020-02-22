c714bf69e2739d91d0705d4e5a4693d472d63243
Chun Yang <canadachunyang@gmail.com>
2020-02-22 16:25:28 -0500

    initial commitment

---
+++ src/components/__tests__/Application.test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Application from "../Application.jsx";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+	render(<Application />);
+});
